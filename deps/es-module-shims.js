d42285b6453a0f216d0329b65caca8649f21b849
Shannon Poole <shannon.m.poole@gmail.com>
2024-03-14 12:26:26 -0400

    Reworked worker-helper and added cache-helper

---
+++ deps/es-module-shims.js
@@ -0,0 +1,1 @@
+import 'https://cdn.jsdelivr.net/npm/es-module-shims@1.8.3/dist/es-module-shims.wasm.js';
---
+++ deps/gl-matrix.js
@@ -0,0 +1,2 @@
+// @deno-types="https://cdn.jsdelivr.net/gh/toji/gl-matrix@v3.4.1/src/types.d.ts"
+export * from 'https://cdn.jsdelivr.net/gh/toji/gl-matrix@v3.4.1/src/index.js';
